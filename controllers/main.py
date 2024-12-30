dd7f31eb5fad976a3d2e0f665d1f58bb10038e81
Oumaima AIT ZAOUIT <oumaima@darbtech.net>
2024-12-30 22:31:19 +0100

    [ADD] dymanic keys settings

--- controllers/main.py
+++ controllers/main.py
@@ -5,102 +5,44 @@ import json
 import logging  
 
 
+class RecaptchaKeys(http.Controller):
+    @http.route('/get_site_key', type='http', auth='public')
+    def get_site_key(self):
+        site_key = request.env['ir.config_parameter'].sudo().get_param('darb_login_signup_recaptcha.site_key')
+        return request.make_response(json.dumps({'site_key': site_key}), [('Content-Type', 'application/json')])
+
+
+
 from odoo.addons.web.controllers.main import Home
 
-class RecaptchaValidationController(Home):
+class RecaptchaValidationControllerLogin(Home):
+
     @http.route('/web/login', type='http', auth="none")
     def web_login(self, redirect=None, **kw):
-        response = super(RecaptchaValidationController, self).web_login(**kw)
-        print("test")
+        response = super(RecaptchaValidationControllerLogin, self).web_login(**kw)
+
         if request.httprequest.method == 'POST':
             client_key = kw.get('g-recaptcha-response')
             if not client_key:
-                return "error': 'reCAPTCHA verification failed. Please try again."
+                return "error : no client key."
             
-            secret_key = '6Lci9qgqAAAAANldIqzXWBavK1B9hn8ja7NTpnEo'
+            
+            secret_key = request.env['ir.config_parameter'].sudo().get_param('darb_login_signup_recaptcha.secret_key')
+            if not secret_key:
+                return "error: 'Secret key not configured.'"
 
             captcha_data = {
                 'secret': secret_key,
                 'response': client_key
             }
             r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=captcha_data)
-            
-            captcha_response = json.loads(r.text)
+
+            captcha_response = r.json()
 
             print(r)
-            print("client_key :", client_key)
+            print("client key :", client_key)
+            print("captcha response :", captcha_response)
     
         return response
     
-
-
-
-
-
-
-
-    # @http.route('/cap', type='http', auth='public', website=True)
-    # def google_recaptcha(self, redirect=None, **kw):
-    #     if request.httprequest.method == 'POST':
-    #         client_key = kw.get('g-recaptcha-response')
-    #         if not client_key:
-    #             return "error': 'reCAPTCHA verification failed. Please try again."
-            
-    #         secret_key = '6Lci9qgqAAAAANldIqzXWBavK1B9hn8ja7NTpnEo'
-
-    #         captcha_data = {
-    #             'secret': secret_key,
-    #             'response': client_key
-    #         }
-    #         r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=captcha_data)
-    #         response = json.loads(r.text)
-
-    #         print(response)
-    #         verify = response.get('success')
-            
-    #         if verify:
-    #             # Logic for successful reCAPTCHA verification
-    #             return request.render('module_name.your_form_template', {
-    #                 'key_1': 'Test',
-    #                 'key_2': kw,
-    #             })
-    #         else:
-    #             # Logic for failed reCAPTCHA verification
-    #             return "error': 'reCAPTCHA verification failed. Please try again."
-            
-
-
-
-        # recaptcha_response = kw.get('g-recaptcha-response')
-        # if not recaptcha_response:           
-        #     return Response(
-        #         json.dumps({'status': 'error', 'message': 'Missing the token'}), 
-        #         content_type='application/json', 
-        #         status=400
-        #     )
-        # else:
-        #     print(f"reCAPTCHA response received: {recaptcha_response}")
-
-        #     _logger = logging.getLogger(__name__)
-        #     _logger.info("reCAPTCHA response received: %s", recaptcha_response)
-
-
-        # secret_key = '6Lci9qgqAAAAANldIqzXWBavK1B9hn8ja7NTpnEo'  
-        # recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify'
-
-        # data = {
-        #     'secret': secret_key,
-        #     'response': recaptcha_response
-        # }
-
-        # response = requests.post(recaptcha_url, data=data)
-        # result = response.json()
-
-        # if result.get('success'):
-        #     return Response(json.dumps({'status': 'success', 'message': 'reCAPTCHA validation successful'}),
-        #                     content_type='application/json', status=200)
-        # else:
-        #     return Response(json.dumps({'status': 'error', 'message': 'reCAPTCHA validation failed'}),
-        #                     content_type='application/json', status=400)
-
     
--- models/res_config_key.py
+++ models/res_config_key.py
@@ -3,23 +3,23 @@ from odoo import fields, models, api, _
 class RecaptchaSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    secret_key = fields.Char(string=_("Clé secrète "))
-    site_key = fields.Char(string=_("Clé de site"))
+    secret_key = fields.Char(string=_("Clé secrète "), config_parameter='darb_login_signup_recaptcha.secret_key')
+    site_key = fields.Char(string=_("Clé de site"), config_parameter='darb_login_signup_recaptcha.site_key')
 
 
 
     def set_values(self):
         super(RecaptchaSettings, self).set_values()
-        self.env['ir.config_parameter'].sudo().set_param('recaptcha.secret_key', self.secret_key)
-        self.env['ir.config_parameter'].sudo().set_param('recaptcha.site_key', self.site_key)
+        self.env['ir.config_parameter'].sudo().set_param('darb_login_signup_recaptcha.secret_key', self.secret_key)
+        self.env['ir.config_parameter'].sudo().set_param('darb_login_signup_recaptcha.site_key', self.site_key)
 
 
     @api.model
     def get_values(self):
         res = super(RecaptchaSettings, self).get_values()
         res.update(
-            secret_key=self.env['ir.config_parameter'].sudo().get_param('recaptcha.secret_key', default=''),
-            site_key=self.env['ir.config_parameter'].sudo().get_param('recaptcha.site_key', default='')
+            secret_key=self.env['ir.config_parameter'].sudo().get_param('darb_login_signup_recaptcha.secret_key'),
+            site_key=self.env['ir.config_parameter'].sudo().get_param('darb_login_signup_recaptcha.site_key')
         )
 
         return res
